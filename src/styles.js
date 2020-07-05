6990d3a8cbdb63a4eff03cc522d085cb5210365b
Aryan Agarwal <aryankush025@gmail.com>
2020-07-06 02:40:32 +0530

    css in js using emotion boilerplate setup

---
+++ src/styles.js
@@ -0,0 +1,17 @@
+import { css } from 'emotion/dist/emotion.umd.min.js'
+
+const brand = '#ff3e00'
+
+export const mainStyles = css`
+	text-align: center;
+	padding: 1em;
+	max-width: 440px;
+	margin: 0 auto;
+`
+
+export const h1Styles = css`
+	color: ${brand};
+	text-transform: uppercase;
+	font-size: 4em;
+	font-weight: 100;
+`
